c82710354cf2851371551c4dbcd16776502d1cac
lucky2007pro <usa20070302@gmail.com>
2026-04-23 10:21:20 +0500

    yuklash

--- crud/urls.py
+++ crud/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-
-from .views import *
+from .views import (
+    UserListCreateView, UserDetailView,
+    CategoryListCreateView, CategoryDetailView,
+    ProductListCreateView, ProductDetailView
+)
 
 urlpatterns = [
-    path('users/', UserListCreateView.as_view()),
-    path('products/', ProductListView.as_view()),
-    path('products/create/', ProductCreateView.as_view()),
-    path('users/<int:pk>/', UserDetailView.as_view()),
+    # User API
+    path('users/', UserListCreateView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+
+    # Category API
+    path('categories/', CategoryListCreateView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+
+    # Product API
+    path('products/', ProductListCreateView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,52 +1,39 @@
-from rest_framework.generics import (
-    ListCreateAPIView,
-    RetrieveUpdateDestroyAPIView
-)
+from rest_framework import generics, filters
+from rest_framework.permissions import IsAuthenticated
 from .models import User, Category, Product
 from .serializers import UserSerializer, CategorySerializer, ProductSerializer
 
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
-class UserListCreateView(ListCreateAPIView):
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs.filter(is_active=True) # (Agar is_active degan maydon bo'lsa)
-
-    def perform_create(self, serializer):
-        serializer.save()
-
-
-class UserDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def perform_update(self, serializer):
-        instance = serializer.save()
-        print(f"{instance.id} id'li foydalanuvchi yangilandi!")
-
-    def perform_destroy(self, instance):
-        print(f"{instance} o'chirib yuborilmoqda...")
-        instance.delete()
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
 
 
-from rest_framework.generics import ListAPIView, CreateAPIView
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
 
-class ProductListView(ListAPIView):
+class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        search_query = self.request.query_params.get('search', None)
-        if search_query:
-            qs = qs.filter(name__icontains=search_query)
-        return qs
 
-class ProductCreateView(CreateAPIView):
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def perform_create(self, serializer):
-        serializer.save()
+    permission_classes = [IsAuthenticated]
